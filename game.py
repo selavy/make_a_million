0d2f70781f1c3be926c4d8abbe5ba8f22a2609de
Peter Lesslie <pclesslie@gmail.com>
2018-11-24 09:13:02 -0600

    Start

---
+++ game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from enum import Enum
+from enum import IntEnum
+from enum import auto
+
+
+class Suit(Enum):
+    RED = auto()
+    YELLOW = auto()
+    BLACK = auto()
+    GREEN = auto()
+
+
