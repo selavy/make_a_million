f64651cbb97a8d328d2028b5cf086e2050af6840
Peter Lesslie <pclesslie@gmail.com>
2018-11-24 11:48:44 -0800

    Sorta getting tests to run

--- src/mmillion/test/test_rules.py
+++ src/mmillion/test/test_rules.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 
-# import mmillion.rules as rules
-import rules
+import mmillion.rules as rules
+# import rules
 
 
 def test_rank_as_str():
     assert rules.rank_as_str(40) == "$40,000"
-    assert rules.rank_as_str(10) == ""
+    assert rules.rank_as_str(30) == "$30,000"    
+    assert rules.rank_as_str(10) == "$10,000"
+    assert rules.rank_as_str(5)  == "$5,000"
+    assert rules.rank_as_str(1)  == "1"
+    assert rules.rank_as_str(3)  == "3"
     
